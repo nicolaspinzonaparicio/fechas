a42e209b89046eb8c8da09f1a0e3b93b24d3fb3d
Semillero SILUX <silux@ufps.edu.co>
2017-10-29 16:10:40 -0500

    base para la clase Dia

---
+++ Dia.py
@@ -0,0 +1,28 @@
+class Dia:
+  
+  # una tupla
+  DIAS_MES = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+  
+  # contrusctor
+  def __init__(self, d, m, a):
+    self.__dia = dia
+    self.__mes = mes
+    self.__anio = a
+  
+  def esBisiesto(self):
+    # nada por aca 
+  
+  def sonIguales(self, otroDia):
+    # nada por aca
+    
+    
+  # metodo set y get
+  
+  
+  def getDia(self):
+    return self.__dia
+  
+  def setDia(self, d):
+    self.__dia = d
+    
+  
