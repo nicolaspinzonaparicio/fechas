ee8b5b49ed98126d99814e6b9e955bf59dd9fe94
sadalsuud <lacasadelduende11@gmail.com>
2017-11-16 16:40:08 -0500

    el main está en el mismo archio . Y aggregué el metodo toString. Y a crea objetos Dia

--- Dia.py
+++ Dia.py
@@ -5,22 +5,27 @@ class Dia:
   
   # contrusctor
   def __init__(self, d, m, a):
-    self.__dia = dia
-    self.__mes = mes
+    self.__dia = d
+    self.__mes = m
     self.__anio = a
   
   def esBisiesto(self):
     # nada por aca 
+    pass
   
   def sonIguales(self, otroDia):
     # nada por aca
+    pass
     
   def esMayor(self, otroDia):
-    pass
     #ESTE ES UN COMENTARIO
     """
     ESTE ES OTRO COMENTARIO
     """
+    pass
+  
+  def toString(self):
+      return str(self.__dia) + "/"+ str(self.__mes) + "/" + str(self.__anio)
   
   # metodo set y get
   
@@ -30,5 +35,12 @@ class Dia:
   
   def setDia(self, d):
     self.__dia = d
+    
+    
+    
+# esto seria el main
+print("Calculdora de fechas bienvenido")
+d = Dia(11,11,2017)
+print("el dia " + d.toString())
     
   
